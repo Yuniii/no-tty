467c398277ec1172f5b1b229a92fc007ed2d8c32
CodinCat <a55951234@gmail.com>
2015-05-26 23:58:33 +0800

    tabs to spaces

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	fs = require('fs'),
-	exec = require('child_process').exec;
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    exec = require('child_process').exec;
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -9,23 +9,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/run', function (req, res) {
-	var className = 'Main',
-		path = __dirname + '/code/',
-		file = path + className + '.java';
+    var className = 'Main',
+        path = __dirname + '/code/',
+        file = path + className + '.java';
 
-	if ( ! fs.existsSync(path)) {
-		fs.mkdirSync(path);
-	}
-	if (fs.existsSync(file)) {
-		fs.unlinkSync(file);
-	}
+    if ( ! fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
+    if (fs.existsSync(file)) {
+        fs.unlinkSync(file);
+    }
 
-	fs.writeFileSync(file, req.body.code);
-	exec('javac ' + file, function (er, o, e) {
-		exec('java -classpath ' + path + ' ' + className, function (err, stdout, stderr) {
-			res.send(stdout);
-		});
-	});
+    fs.writeFileSync(file, req.body.code);
+    exec('javac ' + file, function (er, o, e) {
+        exec('java -classpath ' + path + ' ' + className, function (err, stdout, stderr) {
+            res.send(stdout);
+        });
+    });
 });
 
 app.use('/', express.static(__dirname + '/'));
